6a2d1788a98247bc21bc3b5a086f3c0fa9951995
fhzdaczdac <fhzdaczdac@gmail.com>
2019-07-25 11:16:19 +0800

    刪除BundleConfig

--- HomeWork_1/Global.asax.cs
+++ HomeWork_1/Global.asax.cs
@@ -14,8 +14,7 @@ namespace HomeWork_1
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
